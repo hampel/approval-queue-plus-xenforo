f9ba9ba85db0a84ae12e8561cab0ff2c3573f393
Simon Hampel <simon@hampelgroup.com>
2024-05-13 14:20:01 +1000

    fix bug with upgrade process which saw some columns not being created in some circumstances

--- Setup.php
+++ Setup.php
@@ -14,21 +14,26 @@ class Setup extends AbstractSetup
 		$this->createTables();
 	}
 
-    // upgrade to 3.5.0
-    public function upgrade3050070Step1(array $stepParams = [])
+    // upgrade to 3.5.1
+    public function upgrade3050170Step1(array $stepParams = [])
     {
         if ($this->schemaManager()->tableExists('xf_user_agent'))
         {
             $this->schemaManager()->renameTable('xf_user_agent', 'xf_aqp_user_data');
+            $this->alterTables350();
         }
         elseif ($this->schemaManager()->tableExists('xf_aqp_user_agent'))
         {
             $this->schemaManager()->renameTable('xf_aqp_user_agent', 'xf_aqp_user_data');
-            $this->schemaManager()->alterTable('xf_aqp_user_data', function(Alter $table)
+            $this->alterTables350();
+        }
+        elseif ($this->schemaManager()->tableExists('xf_aqp_user_data'))
+        {
+            // we renamed the table, but didn't create the new columns - oops
+            if (!$this->schemaManager()->columnExists('xf_aqp_user_data', 'iso_code'))
             {
-                $table->addColumn('iso_code', 'varchar', 2)->setDefault('')->after('user_agent');
-                $table->addColumn('cf_location', 'mediumblob')->after('iso_code');
-            });
+                $this->alterTables350();
+            }
         }
         else
         {
@@ -52,4 +57,14 @@ class Setup extends AbstractSetup
 			$table->addPrimaryKey('user_id');
 		});
 	}
+
+    // for v3.5.0+
+    protected function alterTables350()
+    {
+        $this->schemaManager()->alterTable('xf_aqp_user_data', function(Alter $table)
+        {
+            $table->addColumn('iso_code', 'varchar', 2)->setDefault('')->after('user_agent');
+            $table->addColumn('cf_location', 'mediumblob')->after('iso_code');
+        });
+    }
 }
